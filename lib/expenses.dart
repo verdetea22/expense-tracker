b88b6cf657d973b38ab67bf17a8c25667b7bbc6c
mmJDV <mmount@jdvproducts.com>
2024-10-17 10:35:54 -0400

    formatted lists

--- lib/expenses.dart
+++ lib/expenses.dart
@@ -1,4 +1,8 @@
+import 'package:expense_tracker/expenses_list.dart';
+import 'package:expense_tracker/models/expense.dart';
 import 'package:flutter/material.dart';
+
+import 'package:expense_tracker/models/expense.dart';
 
 class Expenses extends StatefulWidget{
   const Expenses({super.key});
@@ -10,14 +14,39 @@ class Expenses extends StatefulWidget{
 }
 
 class _ExpensesState extends State<Expenses>{
+  final List<Expense> _reigsteredExpenses = [
+    Expense(
+      title: 'Flutter Course', 
+      amount: 19.99, 
+      date: DateTime.now(), 
+      category: Category.work,
+    ),
+    Expense(
+      title: 'Scary Movie', 
+      amount: 21.99, 
+      date: DateTime.now(), 
+      category: Category.leisure,
+    ),
+    Expense(
+      title: 'Gas', 
+      amount: 32.30, 
+      date: DateTime.now(), 
+      category: Category.travel,
+    ),
+  ];
+
   @override
   Widget build(BuildContext context){
-    return Scaffold(body: Column(
-      children: [
-        Text('the chart'),
-        Text('expense list')
-      ],
-    ),
+    return Scaffold(
+      // col within a col, need to define dim
+      body: Column(
+        children: [
+          const Text('the chart'),
+          Expanded(
+            child: ExpensesList(expenses: _reigsteredExpenses)
+          )
+        ],
+      ),
     );
   }
 }
--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -2,11 +2,15 @@ import 'package:uuid/uuid.dart';
 
 const uuid = Uuid(); //utility, doesnt belong to class
 
+//custom value, seen as text
+enum Category{food, travel, leisure, work}
+
 class Expense {
   Expense({
     required this.title,
     required this.amount,
     required this.date,
+    required this.category,
   }) : id = uuid.v4(); //random unique id
 // initalizer lists can initalize class properties that arent received as constructor func arg (id)
 
@@ -14,5 +18,5 @@ class Expense {
   final String title;
   final double amount;
   final DateTime date;
-
+  final Category category; 
 }
