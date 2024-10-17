dcb139aeb6da16a5d6a99470376d718aae9495e7
mmJDV <mmount@jdvproducts.com>
2024-10-17 11:19:20 -0400

    file structure, card formatting, added price

--- lib/widgets/expenses_list/expense_item.dart
+++ lib/widgets/expenses_list/expense_item.dart
@@ -9,6 +9,25 @@ class ExpenseItem extends StatelessWidget{
 
   @override
   Widget build(BuildContext context){
-    return Card(child: Text(expense.title),);
+    return Card(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 20,
+          vertical: 16,
+        ),
+        child: Column(
+          children: [
+            Text(expense.title),
+            const SizedBox(height: 4),
+            Row(
+              children: [
+                // "$"" icon + $value
+                Text('\$${expense.amount.toStringAsFixed(2)}'), //double
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
