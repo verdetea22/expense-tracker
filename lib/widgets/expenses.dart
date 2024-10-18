1c12ba61f64bec2117669d156b6358f872ecd857
mmJDV <mmount@jdvproducts.com>
2024-10-18 09:25:14 -0400

    adding new expense card - works

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -39,8 +39,14 @@ class _ExpensesState extends State<Expenses>{
   void _openAddExpenseOverlay() {
     showModalBottomSheet(
       context: context,
-      builder: (ctx) => const NewExpense(),
+      builder: (ctx) => NewExpense(onAddExpense: _addExpense),
     );
+  }
+
+  void _addExpense (Expense expense) {
+    setState(() {
+      _reigsteredExpenses.add(expense);
+    });
   }
 
   @override
